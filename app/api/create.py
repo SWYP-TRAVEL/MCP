a01eed3ec0431566f66796981c535e816fbc5996
김성욱 <swkim@rtst.co.kr>
2025-04-22 10:09:17 +0900

    change name

--- app/api/create.py
+++ app/api/create.py
@@ -1,20 +1,25 @@
 from fastapi import APIRouter, HTTPException
-from schemas import ItineraryDetail
-from clients.client import run
-from agents.mcp import MCPServer, MCPServerSse
-
+from pydantic import BaseModel
+from enum import Enum
+from typing import List, Optional
+from datetime import date
 router = APIRouter(prefix="/api")
+
+class TravelWith(str, Enum):
+    alone   = "alone",
+    friends = "firends",
+    family  = "family",
+    lover   =  "lovel"
+
+class ItineraryDetail(BaseModel):
+    travel_with: TravelWith
+    start_date: date
+    end_date: date
+    description: str
+    
 
 @router.get("/create", summary="itinerary creation request")
 async def create(itinerary_details: ItineraryDetail):
-    async with MCPServerSse(
-        name="SSE Travel Server",
-        params = {
-            "url": "http://localhost:8000/sse",
-        }
-    ) as server:
-        trace_id = gen_trace_id()
-        with trace(workflow_name="SSE Example", trace_id=trace_id):
-            print(f"View trace: https://platform.openai.com/traces/trace?trace_id={trace_id}\n")
-            await run(server)
     return itinerary_details
+
+    
